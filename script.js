66f1404674622c4577dfc98a5096b6f81ad727c4
Peters Gideon Ojoajogu <gideonpeters85@gmail.com>
2018-09-15 16:23:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+var _ = require('lodash');
+
+console.log(_);
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
 function setGradient() {
-	body.style.background = 
+	body.style.background= 
 		"linear-gradient(to right, " 
 		+ color1.value 
 		+ "," 
